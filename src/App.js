a81f149f084c220b9456a0deb76dd071cf96cace
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-02-28 13:12:34 +0200

    create Header, Navbar, Profile components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
-  return (
-    <div className='main'>
-
-      <header className='header'>
-        <img src='https://www.creativefabrica.com/wp-content/uploads/2021/03/20/Mountain-logo-Design-Graphics-9785421-1.png' alt='logo' />
-      </header>
-
-      <nav className='nav'>
-        <ul>
-          <li>
-            <a>Profile</a>
-          </li>
-          <li>
-            <a>Messages</a>
-          </li>
-          <li>
-            <a>News</a>
-          </li>
-          <li>
-            <a>Music</a>
-          </li>
-          <li>
-            <a>Setting</a>
-          </li>
-        </ul>
-      </nav>
-
-      <div className='content'>
-        <h2>Content Block</h2>
-      </div>
-
-    </div>
-  );
-}
+    return (
+        <div className="main">
+            <Header />
+            <Navbar />
+            <Profile />
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://www.creativefabrica.com/wp-content/uploads/2021/03/20/Mountain-logo-Design-Graphics-9785421-1.png"
+                alt="logo"
+            />
+        </header>
+    );
+};
+
+export default Header;
