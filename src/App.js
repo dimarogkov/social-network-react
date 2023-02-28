5e4b74b3f98b9e13129519572b543e8a251b4c08
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-02-28 11:54:19 +0200

    add page structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,37 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div className='main'>
+
+      <header className='header'>
+        <img src='https://www.creativefabrica.com/wp-content/uploads/2021/03/20/Mountain-logo-Design-Graphics-9785421-1.png' alt='logo' />
+      </header>
+
+      <nav className='nav'>
+        <ul>
+          <li>
+            <a>Profile</a>
+          </li>
+          <li>
+            <a>Messages</a>
+          </li>
+          <li>
+            <a>News</a>
+          </li>
+          <li>
+            <a>Music</a>
+          </li>
+          <li>
+            <a>Setting</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className='content'>
+        <h2>Content Block</h2>
+      </div>
+
+    </div>
   );
 }
 
