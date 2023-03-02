b4256329a0ec0ade1a2ad66caa606ab2903e90c0
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-03-02 13:10:08 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
@@ -6,15 +7,19 @@ import Profile from './components/Profile/Profile';
 
 const App = () => {
     return (
-        <div className="main">
-            <Header />
-            <Navbar />
+        <BrowserRouter>
+            <div className="main">
+                <Header />
+                <Navbar />
 
-            <div className="content">
-                <Profile />
-                {/* <Dialogs /> */}
+                <div className="content">
+                    <Routes>
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/dialogs" element={<Dialogs />} />
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,19 +6,19 @@ const Navbar = () => {
         <nav className={s.nav}>
             <ul>
                 <li className={`${s.item} ${s.active}`}>
-                    <a>Profile</a>
+                    <a href="/profile">Profile</a>
                 </li>
                 <li className={s.item}>
-                    <a>Messages</a>
+                    <a href="/dialogs">Messages</a>
                 </li>
                 <li className={s.item}>
-                    <a>News</a>
+                    <a href="/news">News</a>
                 </li>
                 <li className={s.item}>
-                    <a>Music</a>
+                    <a href="/music">Music</a>
                 </li>
                 <li className={s.item}>
-                    <a>Setting</a>
+                    <a href="/setting">Setting</a>
                 </li>
             </ul>
         </nav>
