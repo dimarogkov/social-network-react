3735180e96381947fb394928e8b377fcca5fbf7b
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-03-05 19:18:40 +0200

    add some CSS and JSX to Dialogs Component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,7 +5,27 @@ const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className="container">
-                <h3>Dialogs</h3>
+                <div className={s.dialogs__block}>
+                    <ul className={s.dialogs__people}>
+                        <li className={s.active}>Andre</li>
+                        <li>Dan</li>
+                        <li>Olivia</li>
+                        <li>John</li>
+                    </ul>
+
+                    <div className={s.dialogs__messages}>
+                        <div className={s.dialogs__messages_window}>
+                            <div className={s.dialogs__message}>Hi!</div>
+                            <div className={s.dialogs__message}>How are you?</div>
+                            <div className={`${s.dialogs__message} ${s.dialogs__message_my}`}>Hi! I am fine.</div>
+                        </div>
+
+                        <form className={s.dialogs__messages_navigation}>
+                            <input placeholder="Write message" />
+                            <button>Send</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     );
