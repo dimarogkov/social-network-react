8adb0cf313acae85bcde5ca934def9f4c16a4f3c
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-03-06 15:57:04 +0200

    create DialogsPerson & DialogsMessage Components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import s from './Dialogs.module.css';
+
+const DialogsPerson = (props) => {
+    return (
+        <li>
+            <NavLink to={'/dialogs/' + props.id} className={props.isActive ? s.active : null}>
+                {props.name}
+            </NavLink>
+        </li>
+    );
+};
+
+const DialogsMessage = (props) => {
+    return (
+        <div className={`${s.dialogs__message} ${props.isMyMessage ? s.dialogs__message_my : null}`}>
+            {props.message}
+        </div>
+    );
+};
 
 const Dialogs = () => {
     return (
@@ -7,17 +26,17 @@ const Dialogs = () => {
             <div className="container">
                 <div className={s.dialogs__block}>
                     <ul className={s.dialogs__people}>
-                        <li className={s.active}>Andre</li>
-                        <li>Dan</li>
-                        <li>Olivia</li>
-                        <li>John</li>
+                        <DialogsPerson id="1" name="Daniel" isActive={true} />
+                        <DialogsPerson id="2" name="Dan" isActive={false} />
+                        <DialogsPerson id="3" name="Olivia" isActive={false} />
+                        <DialogsPerson id="4" name="John" isActive={false} />
                     </ul>
 
                     <div className={s.dialogs__messages}>
                         <div className={s.dialogs__messages_window}>
-                            <div className={s.dialogs__message}>Hi!</div>
-                            <div className={s.dialogs__message}>How are you?</div>
-                            <div className={`${s.dialogs__message} ${s.dialogs__message_my}`}>Hi! I am fine.</div>
+                            <DialogsMessage message="Hi!" isMyMessage={false} />
+                            <DialogsMessage message="How are you?" isMyMessage={false} />
+                            <DialogsMessage message="Hi! I am fine." isMyMessage={true} />
                         </div>
 
                         <form className={s.dialogs__messages_navigation}>
